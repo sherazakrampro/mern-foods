a02417f5e7a881322981bcf117d9e1d66b46f475
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-17 15:52:39 +0500

    main navbar

---
+++ frontend/src/components/MainNav.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="text-base font-bold hover:text-orange-500 hover:bg-white"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
