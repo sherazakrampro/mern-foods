958693e4c6a743e9360a0d773c94cab7180266df
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-17 15:41:26 +0500

    mobile navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -10,7 +11,9 @@ const Header = () => {
         >
           MernFoods.com
         </Link>
-        <div className="md:hidden"></div>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
     </div>
   );
