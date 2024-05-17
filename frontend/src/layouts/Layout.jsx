f1e42174b48577c47d0b7e68e4d63f1989c4d42f
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-17 15:29:12 +0500

    header added

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
       <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
   );
