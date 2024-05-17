0dfdc96f3010cfdf4433e4795da8a6dadc089faf
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-17 14:51:13 +0500

    mongodb connection setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
