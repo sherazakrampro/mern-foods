fd8846abc868a6268b609c9f17b218a964584b7b
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-17 17:53:21 +0500

    footer component added

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import PropTypes from "prop-types";
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
       <Header />
       <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 };
