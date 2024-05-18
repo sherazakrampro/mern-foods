05bf9d5546bb84098a34ea738db117dcabfb3a73
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-18 18:21:26 +0500

    user schema & create user controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING)
@@ -13,9 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
