c008356dcce1cbdf80e681d8a0c521f80863fae7
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-18 15:12:58 +0500

    user profile menu & logout functionality

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -1,18 +1,13 @@
+import UsernameMenu from "./UsernameMenu";
 import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav = () => {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <div>
+    <span className="flex space-x-2 items-center">
       {isAuthenticated ? (
-        <Button
-          onClick={logout}
-          variant="ghost"
-          className="text-base font-bold hover:text-orange-500 hover:bg-white"
-        >
-          Log Out
-        </Button>
+        <UsernameMenu />
       ) : (
         <Button
           onClick={loginWithRedirect}
@@ -22,7 +17,7 @@ const MainNav = () => {
           Log In
         </Button>
       )}
-    </div>
+    </span>
   );
 };
 
